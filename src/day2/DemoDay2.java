1641d5f5c4a5f2518defc408d83e2f69a10ab132
carol <carol@Carolina>
2025-01-31 18:13:30 -0500

    Second day practice Git

---
+++ src/day2/DemoDay2.java
@@ -0,0 +1,22 @@
+package day2;
+
+public class DemoDay2 {
+
+	public static void main (String args []) {
+		
+		
+String firstName = "Eliana";
+		
+		System.out.println(firstName);  
+		System.out.println("firstName"); 
+		System.out.println("Tolosa"); 
+		
+		
+	
+		
+	}
+	
+	
+	
+	
+}
